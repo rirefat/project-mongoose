3e01a76d9cbc0dea5891c9d438fdfaea4022dbf7
rirefat <rirefat.official@gmail.com>
2024-05-21 03:38:34 +0600

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const portNumber = process.env.PORT;
+const database = process.env.DATABASE_URL;
